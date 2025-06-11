1bfe6b759d359246b81af7ec40e90c095999cdd7
Squid-Chang-1 <wen870814@gmail.com>
2025-06-12 01:06:46 +0800

    Update teachers.py

--- teachers.py
+++ teachers.py
@@ -1,2 +1,2 @@
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;">獲取教師 ()：</font></font><font></font><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-    print("共有50位老師")</font></font>
+def getTeachers():
+    print("共有 50 位老師")
