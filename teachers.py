f485dff849972959e60ee665eac831305b410ef2
Squid-Chang-1 <wen870814@gmail.com>
2025-06-12 00:41:02 +0800

    Create teachers.py

---
+++ teachers.py
@@ -0,0 +1,2 @@
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;">獲取教師 ()：</font></font><font></font><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+    print("共有50位老師")</font></font>
